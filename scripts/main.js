fc0c210f5573194acadfb440a82ffdebccc2adf3
AugustusPeine <gus.peine@gmail.com>
2025-08-27 17:34:10 -0500

    Changed image

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const myImage = document.querySelector("img");
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/OIP.webp");
+    myImage.setAttribute("src", "images/newFirefox.png");
   } else {
     myImage.setAttribute("src", "images/firefox-icon.png");
   }
